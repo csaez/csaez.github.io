b89920e09f5d16e114ab3ad3eb570e80d17f96fc
Cesar Saez <cesarte@gmail.com>
2019-04-24 19:04:00 +1000

    https ready

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.cesarsaez.me'
+SITEURL = 'https://www.cesarsaez.me'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
